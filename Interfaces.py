7d0be7014dab47268920be0c41938708f440ff13
nombre-usuario <email>
2025-04-03 18:28:15 -0600

    Creación de Función Backend

---
+++ Interfaces.py
@@ -0,0 +1,15 @@
+print("Hola mundo, esta es mi primer interfaz, bienvenido")
+
+a = 45
+b = 875
+c = 74
+
+def multi():
+    print(a*b*c)
+
+multi()
+
+def imprime():
+    print("Esta es la rama para desarrollar el Backend")
+
+imprime()
