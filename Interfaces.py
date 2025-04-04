2ecc4b39109b27dadf1af94e7a6a04f68f8ffd87
nombre-usuario <email>
2025-04-03 18:34:12 -0600

    Crear Función Frontend

---
+++ Interfaces.py
@@ -0,0 +1,15 @@
+print("Hola mundo, esta es mi primer interfaz, bienvenido")
+
+a = 45
+b = 875
+c = 74
+
+def multi():
+    print(a*b*c)
+
+multi()
+
+def mostrar():
+    print("Esta rama está destinada para los cambios necesarios del Frontend")
+
+mostrar()
